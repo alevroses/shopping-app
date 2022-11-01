8aac2174d2fd64353e2056a7f4572b45449b3663
ale vel <ale.vrs@outlook.com>
2022-11-01 15:11:34 -0500

    Insertar detallecompra

---
+++ Appcompras/Datos/Ddetallecompras.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database.Query;
+using System.Linq;
+using Firebase.Database;
+using System.Threading.Tasks;
+using Appcompras.Modelo;
+using Appcompras.Conexiones;
+
+namespace Appcompras.Datos
+{
+    public class Ddetallecompras
+    {
+        public async Task InsertarDc(Mdetallecompras parametros)
+        {
+            await Cconexion.firebase
+                .Child("Detallecompra")
+                .PostAsync(new Mdetallecompras()
+                {
+                    Cantidad = parametros.Cantidad,
+                    Idproducto = parametros.Idproducto,
+                    Preciocompra = parametros.Preciocompra,
+                    Total = parametros.Total
+                });
+        }
+    }
+}
--- Appcompras/VistaModelo/VMagregarcompra.cs
+++ Appcompras/VistaModelo/VMagregarcompra.cs
@@ -1,4 +1,5 @@
-﻿using Appcompras.Modelo;
+﻿using Appcompras.Datos;
+using Appcompras.Modelo;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -41,6 +42,27 @@ namespace Appcompras.VistaModelo
         #endregion
 
         #region PROCESOS
+        public async Task InsertarDc()
+        {
+            if (Cantidad == 0)
+            {
+                Cantidad = 1;
+            }
+            var funcion = new Ddetallecompras();
+            var parametros = new Mdetallecompras();
+            parametros.Cantidad = Cantidad.ToString();
+            parametros.Idproducto = Parametrosrecibe.Idproducto;
+            parametros.Preciocompra = Parametrosrecibe.Precio;
+            double total = 0;
+            double preciocompra = Convert.ToDouble(Parametrosrecibe.Precio);
+            double cantidad = Convert.ToDouble(Cantidad);
+            total = cantidad * preciocompra;
+            parametros.Total = total.ToString();
+
+            await funcion.InsertarDc(parametros);
+            await Volver();
+        }
+
         public async Task Volver()
         {
             await Navigation.PopAsync();
@@ -68,6 +90,7 @@ namespace Appcompras.VistaModelo
         //Llamar al Proceso Simple o no Asincrono
         public ICommand Aumentarcommand => new Command(Aumentar);
         public ICommand Disminuircommand => new Command(Disminuir);
+        public ICommand Insertarcommand => new Command(async () => await InsertarDc());
         #endregion
     }
 }
