f514a6f65dad3e7a09b49f01b3c571f77b0b25ad
ale vel <ale.vrs@outlook.com>
2022-11-01 14:13:06 -0500

    Agregamos un contador de productos

--- Appcompras/VistaModelo/VMagregarcompra.cs
+++ Appcompras/VistaModelo/VMagregarcompra.cs
@@ -11,7 +11,9 @@ namespace Appcompras.VistaModelo
     public class VMagregarcompra:BaseViewModel
     {
         #region VARIABLES
-        string _Texto;
+        int _Cantidad;
+        String _Preciotexto;
+
         public Mproductos Parametrosrecibe { get; set; }
         #endregion
 
@@ -20,35 +22,52 @@ namespace Appcompras.VistaModelo
         {
             Navigation = navigation;
             Parametrosrecibe = parametrosTrae;
+            Preciotexto = "$" + Parametrosrecibe.Precio;
         }
         #endregion
 
         #region OBJETOS
-        public string Texto
+        public string Preciotexto
         {
-            get { return _Texto; }
-            set { SetValue(ref _Texto, value); }
+            get { return _Preciotexto; }
+            set { SetValue(ref _Preciotexto, value); }
+        }
+
+        public int Cantidad
+        {
+            get { return _Cantidad; }
+            set { SetValue(ref _Cantidad, value); }
         }
         #endregion
 
         #region PROCESOS
-        public async Task ProcesoAsyncrono()
+        public async Task Volver()
         {
-
+            await Navigation.PopAsync();
         }
+
         //Cuando no son procesos Asíncronos se
         //remplaza el async Task por void 
-        public void ProcesoSimple()
+        public void Aumentar()
         {
+                Cantidad++;
+        }
 
+        public void Disminuir()
+        {
+            if (Cantidad > 0)
+            {
+                Cantidad--;
+            }
         }
         #endregion
 
         #region COMANDOS
         //Llamar al Proceso Asincrona: await es para tareas asincronas
-        public ICommand ProcesoAsyncommand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand Volvercommand => new Command(async () => await Volver());
         //Llamar al Proceso Simple o no Asincrono
-        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        public ICommand Aumentarcommand => new Command(Aumentar);
+        public ICommand Disminuircommand => new Command(Disminuir);
         #endregion
     }
 }
